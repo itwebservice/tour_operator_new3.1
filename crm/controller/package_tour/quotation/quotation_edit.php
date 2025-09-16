9e6de443b31c862181e594bc3466d1d3f5b9212d
narottamdeveloper-dev <narottam.developer@gmail.com>
2025-09-17 00:44:40 +0530

    git ignore files

---
+++ crm/controller/package_tour/quotation/quotation_edit.php
@@ -0,0 +1,7 @@
+<?php 
+include "../../../model/model.php"; 
+include "../../../model/package_tour/quotation/quotation_edit.php"; 
+
+$quotation_edit = new quotation_edit;
+$quotation_edit->quotation_edit_data();
+?>
