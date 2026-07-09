d21fc13d63893ccfb107ad89431930183d9f6cc2
chauhanjatin <chauhanjatin.25@gmail.com>
2026-07-09 16:35:45 +0530

    AI call implementation(09-7-26)

--- crm/view/dashboard/dashboard_main.php
+++ crm/view/dashboard/dashboard_main.php
@@ -53,6 +53,10 @@ if ($role_id > '7') {
 		}
 	})();
 </script>
+<script src="https://unpkg.com/@elevenlabs/convai-widget-embed" async type="text/javascript"></script>
+<elevenlabs-convai agent-id="agent_1901krgdr6jyedwt06z6grvqgzx0">
+</elevenlabs-convai>
+
 <?php
 /*======******Footer******=======*/
 require_once('../layouts/admin_footer.php');
